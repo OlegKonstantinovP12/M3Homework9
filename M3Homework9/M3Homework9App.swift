931788dee3650abc22f9ff0042e3356b9bd913fe
Oleg Konstantinov <oleg.boss@gmail.com>
2026-01-31 08:40:57 +0100

    Initial Commit

---
+++ M3Homework9/M3Homework9App.swift
@@ -0,0 +1,17 @@
+//
+//  M3Homework9App.swift
+//  M3Homework9
+//
+//  Created by Oleg Konstantinov on 31.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct M3Homework9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
